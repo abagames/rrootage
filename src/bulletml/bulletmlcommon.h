ec05c2f20522ae79988bbd6f401c05e9a3f2df2b
Victor Blanco <vblanco20-1@hotmail.com>
2021-02-20 15:50:38 +0100

    declspec issue fix

--- src/bulletml/bulletmlcommon.h
+++ src/bulletml/bulletmlcommon.h
@@ -1,10 +1,12 @@
 #ifndef bulletmlcommon_h_
 #define bulletmlcommon_h_
 
-#ifdef WIN32_DLL_EXPORT
-# define DECLSPEC __declspec(dllexport)
-#else
-# define DECLSPEC
+#ifdef _WIN32
+	#ifdef WIN32_DLL_EXPORT
+	# define DECLSPEC __declspec(dllexport)
+	#else
+	# define DECLSPEC
+	#endif
 #endif
 
 #include <string.h>
